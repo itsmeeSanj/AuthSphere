8c44c2fa8911c54b6a047429315455aa0ff57196
itsmeeSanj <itsmeesanj@gmail.com>
2026-02-17 14:56:33 -0500

    check/email: existing email register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,31 @@
 // register, login, logout, verifyAccount, PasswordReset, forget password
+
+import bcrypt from "bcryptjs"; //encrypt the password
+import jwt from "jsonwebtoken"; //generate token for authentication
+import userModel from "../models/userModel.js";
+
+export async function register(req, res) {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password)
+    return res.json({
+      success: false,
+      message: "Missing Details",
+    });
+
+  try {
+    // store in database
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser)
+      return res.json({
+        success: false,
+        message: "User already exisit",
+      });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
