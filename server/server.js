3f83648e3c04841f78c1617d5a38772d847da20c
itsmeeSanj <itsmeesanj@gmail.com>
2026-02-24 00:05:08 -0500

    add/authRouters

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookiePraser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authroutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,9 +15,11 @@ app.use(express.json());
 app.use(cookiePraser());
 app.use(cors({ credentials: true })); //send cookies into res
 
+// API ENDPOINTS
 app.get("/", function (req, res) {
   res.send("API is workinng");
 });
+app.use("/api/auth", authRouter);
 
 app.listen(port, function () {
   console.log(`Server started on PORT : ${port}`);
