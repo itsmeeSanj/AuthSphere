ef3dc74191a00c91a63a24aa5d72e0ba7f628b6e
itsmeeSanj <itsmeesanj@gmail.com>
2026-06-04 15:17:18 -0400

    changes

--- client/src/features/admin/components/DashboardHeader.tsx
+++ client/src/features/admin/components/DashboardHeader.tsx
@@ -36,6 +36,7 @@ export default function DashboardHeader({ collapsed, onToggle }: Props) {
       icon: <UserOutlined />,
       label: "Profile",
     },
+
     {
       type: "divider",
     },
--- client/src/layouts/AdminLayout.tsx
+++ client/src/layouts/AdminLayout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Layout, theme } from "antd";
+import { Layout } from "antd";
 import { Outlet } from "react-router";
 import SidebarMenu from "../features/admin/components/SidebarMenu";
 import DashboardHeader from "../features/admin/components/DashboardHeader";
 
-const { Header, Content, Sider } = Layout;
+const { Sider, Content } = Layout;
 
 function AdminLayout() {
   // const { user, logout } = useAuth();
@@ -82,45 +82,11 @@ function AdminLayout() {
             collapsed={collapsed}
             onToggle={() => setCollapsed(!collapsed)}
           />
-          {/* Page content rendered here */}
-          <Outlet />
-        </Layout>
-
-        {/* <Layout>
-          <Header style={{ padding: 0, background: colorBgContainer }}>
-            <Button
-              type='text'
-              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-              onClick={() => setCollapsed(!collapsed)}
-              style={{
-                fontSize: "16px",
-                width: 64,
-                height: 64,
-              }}
-            />
-          </Header>
-
-          <Breadcrumb
-            items={[{ title: "Home" }, { title: "List" }, { title: "App" }]}
-            style={{ padding: "12px  24px" }}
-          />
-
-          <Content style={{ margin: "0 16px", overflow: "initial" }}>
-            <div
-              style={{
-                padding: 24,
-                textAlign: "center",
-                background: colorBgContainer,
-                borderRadius: borderRadiusLG,
-              }}
-            >
-              <Dashboard />
-            </div>
+          <Content style={{ margin: "16px" }}>
+            {/* Page content rendered here */}
+            <Outlet />
           </Content>
-          <Footer style={{ textAlign: "center" }}>
-            Ant Design ©{currentYear} Created by Ant UED
-          </Footer>
-        </Layout> */}
+        </Layout>
       </Layout>
     </>
   );
