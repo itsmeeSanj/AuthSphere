53e8df1779c773c69694ddf00260dcd7cb42aece
itsmeeSanj <itsmeesanj@gmail.com>
2026-06-07 15:08:03 -0400

    refactor: update AdminBreadcrumb component

--- client/src/features/admin/components/AdminBreadcrumb.tsx
+++ client/src/features/admin/components/AdminBreadcrumb.tsx
@@ -1,7 +1,5 @@
-import { Breadcrumb, Typography } from "antd";
+import { Breadcrumb } from "antd";
 import { Link } from "react-router";
-
-const { Title } = Typography;
 
 interface BreadcrumbItem {
   title: string;
@@ -9,15 +7,14 @@ interface BreadcrumbItem {
 }
 
 interface Props {
-  title: string;
   breadcrumbs: BreadcrumbItem[];
 }
 
-export default function AdminBreadcrumb({ title, breadcrumbs }: Props) {
+export default function AdminBreadcrumb({ breadcrumbs }: Props) {
   return (
     <>
       <Breadcrumb
-        // style={{ marginBottom: 8 }}
+        style={{ marginBottom: 16 }}
         items={breadcrumbs.map((item) => ({
           title: item.path ? (
             <Link to={item.path}>{item.title}</Link>
@@ -26,27 +23,6 @@ export default function AdminBreadcrumb({ title, breadcrumbs }: Props) {
           ),
         }))}
       />
-
-      {/*  */}
-
-      <div
-        className='rounded-sm my-4 p-4'
-        style={{
-          background: "linear-gradient(135deg, #6367FF 0%, #33369b 100%)",
-        }}
-      >
-        <h5 className='text-white font-bold capitalize'>{title} </h5>
-        {/* <Title level={3} style={{ color: "#fff", margin: 0 }}>
-          Welcome back, {user?.name}! 👋
-        </Title>
-        <Text style={{ color: "rgba(255,255,255,0.75)" }}>
-          {user?.email} · {user?.role ?? "admin"}
-        </Text> */}
-      </div>
-
-      {/* <Title level={4} style={{ margin: "16px 0 " }}>
-        {title}
-      </Title> */}
     </>
   );
 }
