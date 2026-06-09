0186bb87bc33e456c7f58d1d125d88188f6e3df8
itsmeeSanj <itsmeesanj@gmail.com>
2026-06-09 15:07:14 -0400

    refactor: update Dashboard page

--- client/src/features/admin/pages/Dashboard.tsx
+++ client/src/features/admin/pages/Dashboard.tsx
@@ -85,7 +85,7 @@ export default function Dashboard() {
     <>
       <AdminBreadcrumb
         breadcrumbs={[
-          { title: "Home", path: "/admin/dashboard" },
+          { title: "Home", path: "/admin" },
           { title: "Dashboard" },
         ]}
       />
