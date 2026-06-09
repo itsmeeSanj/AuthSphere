145693dc7f3a73fd8f2a1816725a4d36995c2b89
itsmeeSanj <itsmeesanj@gmail.com>
2026-06-09 15:03:22 -0400

    feat: add /all and /delete/:userId routes to userRouter

--- server/routes/userRouters.js
+++ server/routes/userRouters.js
@@ -3,6 +3,8 @@ import express from "express";
 import userAuth from "../middleware/userAuth.js";
 import {
   getUserData,
+  getAllUsers,
+  deleteAccount,
   //   getUserStats,
   getProfile,
   updateProfile,
@@ -12,6 +14,11 @@ import {
 const userRouter = express.Router();
 
 userRouter.get("/data", userAuth, getUserData);
+
+userRouter.get("/all", userAuth, getAllUsers);
+userRouter.delete("/delete/:userId", userAuth, deleteAccount); // admin deletes user
+userRouter.delete("/delete", userAuth, deleteAccount);
+
 // userRouter.get("/stats", userAuth, getUserStats);
 userRouter.get("/profile", userAuth, getProfile);
 userRouter.put("/profile", userAuth, updateProfile);
