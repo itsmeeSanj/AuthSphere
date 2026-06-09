1b90d47df6750a1be4915a4d50ff97ba0d50b425
itsmeeSanj <itsmeesanj@gmail.com>
2026-06-09 14:59:59 -0400

    feat: add timestamps to User model for createdAt and updatedAt

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
 
-  verifyOtp: { type: String, default: "" },
-  verifyOtpExpireAT: { type: Number, default: 0 },
-  isAccountVerified: { type: Boolean, default: false }, // default will be unverified
-  resetOtp: { type: String, default: "" }, // default will be unverified
-  resetOtpExpireAT: { type: Number, default: 0 },
-});
+    verifyOtp: { type: String, default: "" },
+    verifyOtpExpireAT: { type: Number, default: 0 },
+    isAccountVerified: { type: Boolean, default: false }, // default will be unverified
+    resetOtp: { type: String, default: "" }, // default will be unverified
+    resetOtpExpireAT: { type: Number, default: 0 },
+  },
+  { timestamps: true }, //← adds createdAt and updatedAt automatically
+);
 
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 
